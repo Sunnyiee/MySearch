cb07b8856d6ae0fda90fb9d23827551a4b95a2a1
Sunnyiee <rlatjsdl002@gmail.com>
2018-11-21 15:45:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/edu/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.edu.myapplication;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button=findViewById(R.id.searchButton);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText siteUrl=(EditText)findViewById(R.id.siteUrl);
+        WebView webView=(WebView)findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+        webView.loadUrl(siteUrl.getText().toString());
+        webView.requestFocus();
+    }
+}
